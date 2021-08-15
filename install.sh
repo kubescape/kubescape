bc61755f67483e9e957a0e371b50285aecec67aa
Benyamin Hirschberg <59160382+BenHirschbergCa@users.noreply.github.com>
2021-08-15 21:46:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo
  
 BASE_DIR=~/.kubescape
 KUBESCAPE_EXEC=kubescape
-RELEASE=v0.0.12
+RELEASE=v0.0.18
 DOWNLOAD_URL="https://github.com/armosec/kubescape/releases/download/$RELEASE/kubescape"
 
 mkdir -p $BASE_DIR 
