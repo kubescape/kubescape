5f5215ff8edfea29e301e5baf6e74693f6b2fedc
dwertent <dwertent@cyberarmor.io>
2021-08-21 23:13:27 +0300

    update install

--- install.sh
+++ install.sh
@@ -7,7 +7,10 @@ echo
 BASE_DIR=~/.kubescape
 KUBESCAPE_EXEC=kubescape
 
-DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/armosec/kubescape/releases/latest" | grep -o "browser_download_url.*")
+# GITHUB_ORG=armosec
+GITHUB_ORG=dwertent
+
+DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/$GITHUB_ORG/kubescape/releases/latest" | grep -o "browser_download_url.*")
 DOWNLOAD_URL=${DOWNLOAD_URL//\"}
 DOWNLOAD_URL=${DOWNLOAD_URL/browser_download_url: /}
 
