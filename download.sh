67c2de74f1199bfe3479caa5208802c21531640f
dwertent <dwertent@cyberarmor.io>
2021-08-31 17:05:16 +0300

    adding download script

---
+++ download.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+echo "Downloading Kubescape..."
+echo
+ 
+BASE_DIR=~/.kubescape
+KUBESCAPE_EXEC=kubescape
+
+osName=$(uname -s)
+if [[ $osName == *"MINGW"* ]]; then
+    osName=windows-latest
+elif [[ $osName == *"Darwin"* ]]; then
+    osName=macos-latest
+else
+    osName=ubuntu-latest
+fi
+
+GITHUB_OWNER=armosec
+
+DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/$GITHUB_OWNER/kubescape/releases/latest" | grep -o "browser_download_url.*${osName}.*")
+DOWNLOAD_URL=${DOWNLOAD_URL//\"}
+DOWNLOAD_URL=${DOWNLOAD_URL/browser_download_url: /}
+
+mkdir -p $BASE_DIR 
+
+OUTPUT=$BASE_DIR/$KUBESCAPE_EXEC
+
+curl --progress-bar -L $DOWNLOAD_URL -o $OUTPUT
+echo -e "\033[32m[V] Downloaded Kubescape"
+
+chmod +x $OUTPUT || sudo chmod +x $OUTPUT
