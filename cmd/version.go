8375a8ae630dc6cc7fd17da6774c2635661c23d9
Daniel-GrunbergerCA <danielg@armosec.io>
2021-09-09 14:37:25 +0300

    add version handling

--- cmd/version.go
+++ cmd/version.go
@@ -1,14 +1,18 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
 
 var (
-	sha1ver   string
-	buildTime string
+	sha1ver    string
+	buildTime  string
+	run_number string
 )
 
 // versionCmd represents the version command
@@ -16,12 +20,44 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Get current version",
 	Long:  ``,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version")
+	RunE: func(cmd *cobra.Command, args []string) error {
 		fmt.Println(sha1ver)
 		fmt.Println(buildTime)
-
+		latest, err := GetLatestVersion()
+		if err != nil {
+			return err
+		}
+		fmt.Println(latest)
+		if latest == run_number {
+			fmt.Println("Updated")
+		} else {
+			fmt.Println("Not updated")
+		}
+		return nil
 	},
+}
+
+func GetLatestVersion() (string, error) {
+	latestVersion := "https://api.github.com/repos/armosec/kubescape/releases/latest"
+	resp, err := http.Get(latestVersion)
+	if err != nil {
+		return "", fmt.Errorf("failed to get latest releases from '%s', reason: %s", latestVersion, err.Error())
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode < 200 || 301 < resp.StatusCode {
+		return "", fmt.Errorf("failed to download file, status code: %s", resp.Status)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body from '%s', reason: %s", latestVersion, err.Error())
+	}
+	var data map[string]interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return "", fmt.Errorf("failed to unmarshal response body from '%s', reason: %s", latestVersion, err.Error())
+	}
+	return fmt.Sprintf("%v", data["tag_name"]), nil
 }
 
 func init() {
--- main.go
+++ main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/armosec/kubescape/cmd"
 )
 
-var (
-	sha1ver   string
-	buildTime string
-)
-
 func main() {
-	fmt.Println(sha1ver)
-	fmt.Println(buildTime)
+
 	cmd.Execute()
 }
