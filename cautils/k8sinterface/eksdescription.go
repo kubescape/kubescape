e9d9cbd8f5fb9bb5188c54824ebf0137cca999e1
Venessa <>
2021-10-08 08:30:15 -0400

    Return the first cluster from a list of all of the clusters in the active context

--- cautils/k8sinterface/eksdescription.go
+++ cautils/k8sinterface/eksdescription.go
@@ -14,4 +14,27 @@ func GetEKSClusterDescription() {
 		SharedConfigState: session.SharedConfigEnable,
 	})))
 
+	//Return the first cluster from a list of all of the clusters in the active context
+	list := &eks.ListClustersInput{}
+	firstCluster, err := svc.ListClusters(list)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case eks.ErrCodeResourceNotFoundException:
+				fmt.Println(eks.ErrCodeResourceNotFoundException, aerr.Error())
+			case eks.ErrCodeClientException:
+				fmt.Println(eks.ErrCodeClientException, aerr.Error())
+			case eks.ErrCodeServerException:
+				fmt.Println(eks.ErrCodeServerException, aerr.Error())
+			case eks.ErrCodeServiceUnavailableException:
+				fmt.Println(eks.ErrCodeServiceUnavailableException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			fmt.Println(err.Error())
+		}
+		return
+	}
+	fmt.Println(*firstCluster.Clusters[0])
 }
