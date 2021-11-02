9a1b8d7ce2cf0c24ae2e76f8070608785c9ed890
dwertent <dwertent@cyberarmor.io>
2021-11-02 16:14:09 +0200

    support submit

--- README.md
+++ README.md
@@ -174,6 +174,24 @@ kubescape scan framework nsa --use-from nsa.json
 
 Kubescape is an open source project, we welcome your feedback and ideas for improvement. We’re also aiming to collaborate with the Kubernetes community to help make the tests themselves more robust and complete as Kubernetes develops.
 
+# Submit data manually
+
+Use the `submit` command if you wish to submit data manually
+
+## Submit Role-Based Access Control (RBAC)
+```
+kubescape submit rbac
+```
+
+
+## Submit scan results manually
+
+First, scan your cluster using the `json` format flag: `kubescape scan framework <name> --format json --output path/to/results.json`.
+
+Now you can submit the results to the Kubaescape SaaS version -
+```
+kubescape submit results path/to/results.json
+```
 # How to build
 
 ## Build using python (3.7^) script
--- smoke_testing/init.py
+++ smoke_testing/init.py
@@ -1,11 +1,25 @@
+"""
+Kubescape smoke testing
+
+Execute all tests:
+python init.py path/the/bin/kubescape
+
+Execute single test:
+python test_<name>.py path/the/bin/kubescape
+
+Add a new test:
+1. Create a python file with test_ prefix
+2. Implement a function named run()
+
+"""
+
 import sys
 import smoke_utils
+import glob
+import os
 
-
-tests_pkg = [
-    "test_command"
-    , "test_version"
-]
+# get all python files in dir that begin with test_
+tests_pkg = list(map(lambda x: os.path.splitext(os.path.basename(x))[0], glob.glob(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_*.py'))))
 
 
 def run(**kwargs):
@@ -15,4 +29,14 @@ def run(**kwargs):
 
 
 if __name__ == "__main__":
+    # the first argument should be the kubescape binary path
     run(kubescape_exec=smoke_utils.get_exec_from_args(sys.argv))
+
+'''
+Tests to add:
+1. Scan E2E
+2. Download and add --use-from
+
+3. Test formats + output
+4. Test -t
+'''
