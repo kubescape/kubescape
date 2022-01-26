a96652094e51dcf96ad9bb073cbc5b5fbaeb36b0
Wim de Groot <34519486+degrootwim@users.noreply.github.com>
2022-01-26 14:57:19 +0100

    Added length check to prevent panic (#333)
    Signed-off-by: Wim de Groot <34519486+degrootwim@users.noreply.github.com>

--- resultshandling/printer/v1/junit.go
+++ resultshandling/printer/v1/junit.go
@@ -107,7 +107,7 @@ func convertPostureReportToJunitResult(postureResult *reporthandling.PostureRepo
 			testCase.Name = controlReports.Name
 			testCase.Classname = "Kubescape"
 			testCase.Time = postureResult.ReportGenerationTime.String()
-			if 0 < len(controlReports.RuleReports[0].RuleResponses) {
+			if 0 < len(controlReports.RuleReports) && 0 < len(controlReports.RuleReports[0].RuleResponses) {
 
 				testCase.Resources = controlReports.GetNumberOfResources()
 				testCase.Excluded = controlReports.GetNumberOfWarningResources()
