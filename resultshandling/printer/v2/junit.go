544a19906e149086341235818794db26f4c7cc2a
dwertent <dwertent@cyberarmor.io>
2022-02-27 17:29:42 +0200

    update junit

--- resultshandling/printer/v2/junit.go
+++ resultshandling/printer/v2/junit.go
@@ -10,11 +10,50 @@ import (
 	"github.com/armosec/kubescape/cautils/logger"
 	"github.com/armosec/kubescape/cautils/logger/helpers"
 	"github.com/armosec/kubescape/resultshandling/printer"
+	"github.com/armosec/opa-utils/reporthandling/results/v1/reportsummary"
 )
 
 type JunitPrinter struct {
 	writer  *os.File
 	verbose bool
+}
+
+type JUnitTestSuites struct {
+	XMLName    xml.Name          `xml:"testsuite"`
+	Suites     []JUnitTestCase   `xml:"testsuites"`
+	Frameworks []JUnitFrameworks `xml:"framework"`
+	RiskScore  float32           `xml:"riskScore,attr"` // test risk score
+	Time       string            `xml:"time,attr"`      // scanning time
+	Controls   int               `xml:"testcases,attr"` // number of controls
+}
+
+type JUnitFrameworks struct { // Frameworks
+	Name      string  `xml:"name,attr"`
+	RiskScore float32 `xml:"riskscore,attr"`
+	Status    string  `xml:"status,attr"`
+}
+
+// JUnitTestCase is a single test case with its result.
+type JUnitTestCase struct { // Control
+	XMLName      xml.Name        `xml:"testcase"`
+	Name         string          `xml:"name,attr"`
+	ID           string          `xml:"id,attr"`
+	Url          string          `xml:"url,attr"`
+	RiskScore    float32         `xml:"riskScore,attr"`
+	Status       string          `xml:"status,attr"`
+	Info         string          `xml:"info,attr"`
+	AllResources int             `xml:"allResources,attr"`
+	Excluded     int             `xml:"excludedResources,attr"`
+	Failed       int             `xml:"filedResources,attr"`
+	Resources    []JUnitResource `xml:"resource"`
+}
+
+type JUnitResource struct { // Single resource
+	Name        string                   `xml:"name,attr"`
+	Namespace   string                   `xml:"namespace,attr"`
+	Kind        string                   `xml:"kind,attr"`
+	ApiVersion  string                   `xml:"apiVersion,attr"`
+	FailedPaths []armotypes.PosturePaths `xml:"jsonpath"`
 }
 
 func NewJunitPrinter(verbose bool) *JunitPrinter {
@@ -47,73 +86,66 @@ func (junitPrinter *JunitPrinter) ActionPrint(opaSessionObj *cautils.OPASessionO
 	junitPrinter.writer.Write(postureReportStr)
 }
 
-type JUnitTestSuites struct {
-	XMLName   xml.Name        `xml:"testsuite"`
-	Suites    []JUnitTestCase `xml:"testsuites"`
-	RiskScore float32         `xml:"riskScore,attr"` // test risk score
-	Time      string          `xml:"time,attr"`      // scanning time
-	Controls  int             `xml:"tests,attr"`     // number of controls
-}
-
-// JUnitTestCase is a single test case with its result.
-type JUnitTestCase struct { // Control
-	XMLName      xml.Name        `xml:"testcase"`
-	RiskScore    float32         `xml:"riskScore,attr"`
-	Status       string          `xml:"status,attr"`
-	Name         string          `xml:"name,attr"`
-	AllResources int             `xml:"allResources,attr"`
-	Excluded     int             `xml:"excludedResources,attr"`
-	Failed       int             `xml:"filedResources,attr"`
-	Resources    []JUnitResource `xml:"resources"`
-}
-
-type JUnitResource struct { // Single resource
-	Name        string                   `xml:"name,attr"`
-	Namespace   string                   `xml:"namespace,attr"`
-	Kind        string                   `xml:"kind,attr"`
-	ApiVersion  string                   `xml:"apiVersion,attr"`
-	FailedPaths []armotypes.PosturePaths `xml:"jsonPaths"`
-}
-
 func (junitPrinter *JunitPrinter) convertPostureReportToJunitResult(results *cautils.OPASessionObj) (*JUnitTestSuites, error) {
 	juResult := JUnitTestSuites{
 		XMLName: xml.Name{
 			Local: "Kubescape scan results",
 		},
-		RiskScore: results.Report.SummaryDetails.Score,
-		Time:      results.Report.GetTimestamp().String(),
-		Controls:  len(results.Report.ListControls().All()),
+		RiskScore:  results.Report.SummaryDetails.Score,
+		Time:       results.Report.GetTimestamp().String(),
+		Controls:   len(results.Report.ListControls().All()),
+		Frameworks: []JUnitFrameworks{},
+	}
+
+	// Frameworks
+	for _, frameworksReports := range results.Report.ListFrameworks().All() {
+		fw := JUnitFrameworks{}
+		fw.Name = frameworksReports.GetName()
+		fw.RiskScore = frameworksReports.GetScore()
+		fw.Status = string(frameworksReports.GetStatus().Status())
+		juResult.Frameworks = append(juResult.Frameworks, fw)
 	}
 
 	// controls
-	for _, controlReports := range results.Report.ListControls().All() {
+	for _, controlIDs := range results.Report.ListControlsIDs().All() {
+		controlReport := results.Report.SummaryDetails.Controls.GetControl(reportsummary.EControlCriteriaID, controlIDs)
+
+		// control data
 		testCase := JUnitTestCase{}
-		testCase.Name = controlReports.GetName()
-		testCase.Status = string(controlReports.GetStatus().Status())
+		testCase.Name = controlReport.GetName()
+		testCase.ID = controlReport.GetID()
+		testCase.Url = getControlURL(controlReport.GetID())
+		testCase.Name = controlReport.GetName()
+		testCase.Status = string(controlReport.GetStatus().Status())
+
+		// resources counters
+		testCase.AllResources = controlReport.NumberOfResources().All()
+		testCase.Excluded = controlReport.NumberOfResources().Excluded()
+		testCase.Failed = controlReport.NumberOfResources().Failed()
 
 		// resources
-		testCase.AllResources = controlReports.NumberOfResources().All()
-		testCase.Excluded = controlReports.NumberOfResources().Excluded()
-		testCase.Failed = controlReports.NumberOfResources().Failed()
-
 		var jUnitResources []JUnitResource
-		for _, resourceID := range controlReports.ListResourcesIDs().All() {
-			if !junitPrinter.verbose {
+		for _, resourceID := range controlReport.ListResourcesIDs().All() {
+			result, ok := results.ResourcesResult[resourceID]
+			if !ok {
 				continue
 			}
+			if result.GetStatus(nil).IsPassed() && !junitPrinter.verbose { // add passed resources only in verbose mode
+				continue
+			}
+
 			jUnitResource := JUnitResource{}
+			rules := result.ListRulesOfControl(controlReport.GetID(), "")
+			for _, rule := range rules {
+				jUnitResource.FailedPaths = append(jUnitResource.FailedPaths, rule.Paths...)
+			}
 			if resource, ok := results.AllResources[resourceID]; ok {
 				jUnitResource.Name = resource.GetName()
 				jUnitResource.Namespace = resource.GetNamespace()
 				jUnitResource.Kind = resource.GetKind()
 				jUnitResource.ApiVersion = resource.GetApiVersion()
 			}
-			if result, ok := results.ResourcesResult[resourceID]; ok {
-				rules := result.ListRulesOfControl("", controlReports.GetName())
-				for _, rule := range rules {
-					jUnitResource.FailedPaths = append(jUnitResource.FailedPaths, rule.Paths...)
-				}
-			}
+
 			jUnitResources = append(jUnitResources, jUnitResource)
 		}
 		testCase.Resources = jUnitResources
