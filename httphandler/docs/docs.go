96903ea77dfa868231cecd67d2d3dd9945d48276
Vlad Klokun <vklokun@protonmail.ch>
2022-07-20 20:08:14 +0300

    docs: swagger: add OpenAPI V2 schema

---
+++ httphandler/docs/docs.go
@@ -0,0 +1,24 @@
+// Package classification kubescape_microservice
+//
+// Documentation of our awesome API.
+//
+//      Schemes: http
+//      BasePath: /
+//      Version: 1.0.0
+//      Host: example.com
+//
+//      Consumes:
+//      - application/json
+//
+//      Produces:
+//      - application/json
+//
+//      Security:
+//      - basic
+//
+//      SecurityDefinitions:
+//      basic:
+//        type: basic
+//
+// swagger:meta
+package docs
