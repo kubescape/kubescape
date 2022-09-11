f48b848eb62d81682dbcac533801ee5974498cfa
Cheithanya <cheithanya2002@gmail.com>
2022-09-11 23:48:14 +0530

    auto pick the OS of the usersystem

--- cmd/update/update.go
+++ cmd/update/update.go
@@ -11,7 +11,7 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
-	"strings"
+	"runtime"
 
 	"github.com/kubescape/kubescape/v2/core/cautils"
 	"github.com/spf13/cobra"
@@ -22,8 +22,7 @@ func GetUpdateCmd() *cobra.Command {
 		Use:   "update",
 		Short: "Update your version",
 		Long:  ``,
-		Args:  cobra.ExactValidArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
+		RunE: func(_ *cobra.Command, args []string) error {
 			vu := cautils.NewIVersionCheckHandlerU()
 			vu.CheckLatestVersionU(cautils.NewVersionCheckRequestU(cautils.BuildNumber, "", "", "version"))
 			//Checking the user's version of kubescape to the latest release
@@ -32,23 +31,23 @@ func GetUpdateCmd() *cobra.Command {
 				fmt.Println("You are in the latest version")
 			} else {
 				//execute the install.sh if linux, install.ps1 for windows,.....depending on your OS
-				switch strings.ToLower(args[0]) {
-				case "linux":
+				var OSTYPE string = runtime.GOOS
+				if OSTYPE == "linux" {
+
 					//run the installation command for linux
 					cmd, err := exec.Command("./install.sh").Output()
 					if err != nil {
 						log.Fatal(err)
 					}
 					fmt.Println(string(cmd))
-				case "windows":
+				} else if OSTYPE == "windows" {
 					//run the installation command for windows
 					cmd, err := exec.Command("./install.ps1").Output()
 					if err != nil {
 						log.Fatal(err)
 					}
 					fmt.Println(string(cmd))
-
-				case "macos":
+				} else {
 					//run the installation command for macOS
 					cmd, err := exec.Command("./macinstall/kubescape.rb").Output()
 					if err != nil {
