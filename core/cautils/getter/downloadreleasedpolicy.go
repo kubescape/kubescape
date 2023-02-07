4ea35eec00c05b7c30fe6c6f1e748973f1f996b4
kooomix <emadar@gmail.com>
2023-02-07 13:18:51 +0200

    GitRegoStore moved to Regolibrary (#1058)
    * GitRegoStore moved to Regolibrary
    
    * httphandler go mod
    
    * update go.mod
    
    * test old regostorlibrary
    
    * restore gitRegoStore from regolibrary
    
    * Update 	github.com/kubescape/regolibrary to ver 249
    
    Signed-off-by: kooomix <eranm@armosec.io>
    
    * update go mod
    
    Signed-off-by: kooomix <eranm@armosec.io>
    
    * print scan_control_id mesg
    
    Signed-off-by: kooomix <eranm@armosec.io>
    
    * Update regolibrary version - windows host fix
    
    Signed-off-by: kooomix <eranm@armosec.io>
    
    ---------
    
    Signed-off-by: kooomix <eranm@armosec.io>
    Co-authored-by: David Wertenteil <dwertent@armosec.io>

--- core/cautils/getter/downloadreleasedpolicy.go
+++ core/cautils/getter/downloadreleasedpolicy.go
@@ -5,9 +5,10 @@ import (
 	"strings"
 
 	"github.com/armosec/armoapi-go/armotypes"
-	"github.com/kubescape/opa-utils/gitregostore"
 	"github.com/kubescape/opa-utils/reporthandling"
 	"github.com/kubescape/opa-utils/reporthandling/attacktrack/v1alpha1"
+
+	"github.com/kubescape/regolibrary/gitregostore"
 )
 
 // =======================================================================================================================
