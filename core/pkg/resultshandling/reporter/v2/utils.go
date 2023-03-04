6c54aff451e474c8e55ec62d4089c0bcc1dc77e0
David Wertenteil <dwertent@armosec.io>
2023-03-04 22:46:20 +0200

    wip: removed unused code
    Signed-off-by: David Wertenteil <dwertent@armosec.io>

--- core/pkg/resultshandling/reporter/v2/utils.go
+++ core/pkg/resultshandling/reporter/v2/utils.go
@@ -5,26 +5,6 @@ import (
 	"strings"
 )
 
-/* unused for now
-func maskID(id string) string {
-	sep := "-"
-	splitted := strings.Split(id, sep)
-	if len(splitted) != 5 {
-		return ""
-	}
-	str := splitted[0][:4]
-	splitted[0] = splitted[0][4:]
-	for i := range splitted {
-		for j := 0; j < len(splitted[i]); j++ {
-			str += "X"
-		}
-		str += sep
-	}
-
-	return strings.TrimSuffix(str, sep)
-}
-*/
-
 func parseHost(urlObj *url.URL) {
 	if strings.Contains(urlObj.Host, "http://") {
 		urlObj.Scheme = "http"
