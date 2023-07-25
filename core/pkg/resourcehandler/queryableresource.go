aab9cd5ff905fd3c1abdb1d768dc2ae981d456b1
Amir Malka <amirm@armosec.io>
2023-07-25 15:24:09 +0300

    added unit tests
    Signed-off-by: Amir Malka <amirm@armosec.io>

--- core/pkg/resourcehandler/queryableresource.go
+++ core/pkg/resourcehandler/queryableresource.go
@@ -6,6 +6,16 @@ import (
 	"github.com/kubescape/kubescape/v2/core/cautils"
 )
 
+type QueryableResources map[string]QueryableResource
+
+// QueryableResource is a struct that holds a representation of a resource we would like to query (from the K8S API, or from other sources)
+type QueryableResource struct {
+	// <api group/api version/resource>
+	GroupVersionResourceTriplet string
+	// metadata.name==<resource name>, metadata.namespace==<resource namespace> etc.
+	FieldSelectors string
+}
+
 func (qr *QueryableResource) String() string {
 	if qr.FieldSelectors == "" {
 		return qr.GroupVersionResourceTriplet
