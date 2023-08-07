f95ea0bb08b5b1b4b18287c8bc5f16032a0d6321
Ben <ben@armosec.io>
2023-08-07 22:39:03 +0300

    fix test
    Signed-off-by: Ben <ben@armosec.io>

--- core/pkg/opaprocessor/normalize_image_name_test.go
+++ core/pkg/opaprocessor/normalize_image_name_test.go
@@ -21,7 +21,8 @@ func Test_normalize_name(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, has_signature(tt.img), tt.name)
+			name, _ := normalize_image_name(tt.img)
+			assert.Equal(t, tt.want, name, tt.name)
 		})
 	}
 }
