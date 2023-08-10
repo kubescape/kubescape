1d6452260790aa251df1c2b00e00d55a9aeb3aeb
Matthias Bertschy <matthias.bertschy@gmail.com>
2023-08-10 14:46:07 +0200

    use distroless base image (#1338)
    * use distroless base image
    
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>
    
    * bump cosign to v2
    
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>
    
    ---------
    
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

--- core/pkg/opaprocessor/cosign_has_signature.go
+++ core/pkg/opaprocessor/cosign_has_signature.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"github.com/google/go-containerregistry/pkg/name"
-	"github.com/sigstore/cosign/pkg/cosign"
+	"github.com/sigstore/cosign/v2/pkg/cosign"
 )
 
 func has_signature(img string) bool {
