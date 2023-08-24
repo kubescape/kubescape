be8f02cf46307bf327677e5f377f211e074d8b6b
David Wertenteil <dwertent@armosec.io>
2023-08-24 15:03:48 +0300

    refactor script
    Signed-off-by: David Wertenteil <dwertent@armosec.io>

--- docs/providers/install.sh
+++ docs/providers/install.sh
@@ -1,42 +1,50 @@
 #!/bin/bash
 set -e
 
-while getopts v: option
-do
+BASE_DIR=~/.kubescape
+KUBESCAPE_EXEC=kubescape
+
+# Function to determine OS and architecture
+determine_os_and_arch() {
+    osName=$(uname -s)
+    case $osName in
+        *MINGW*) osName=windows ;;
+        Darwin*) osName=macos ;;
+        *) osName=ubuntu ;;
+    esac
+
+    arch=$(uname -m)
+    case $arch in
+        *aarch64*|*arm64*) arch="-arm64" ;;
+        *x86_64*) arch="" ;;
+        *) 
+            echo -e "\033[33mArchitecture $arch may be unsupported, will try to install the amd64 one anyway."
+            arch=""
+            ;;
+    esac
+}
+
+# Function to remove old installations
+remove_old_install() {
+    local exec_path=$1
+    if [ -f "$exec_path" ]; then
+        rm -f "$exec_path" && echo -e "\033[32mRemoved old installation at $exec_path" || echo -e "\033[31mFailed to remove old installation at $exec_path"
+    fi
+}
+
+# Parse command-line arguments
+while getopts v: option; do
     case ${option} in
         v) RELEASE="download/${OPTARG}";;
         *) ;;
     esac
 done
 
-if [ -z "${RELEASE}" ]; then
-    RELEASE="latest/download"
-fi
+[ -z "${RELEASE}" ] && RELEASE="latest/download"
 
 echo -e "\033[0;36mInstalling Kubescape..."
-echo
 
-BASE_DIR=~/.kubescape
-KUBESCAPE_EXEC=kubescape
-
-osName=$(uname -s)
-if [[ $osName == *"MINGW"* ]]; then
-    osName=windows
-elif [[ $osName == *"Darwin"* ]]; then
-    osName=macos
-else
-    osName=ubuntu
-fi
-
-arch=$(uname -m)
-if [[ $arch == *"aarch64"* || $arch == *"arm64"* ]]; then
-    arch="-arm64"
-else
-    if [[ $arch != *"x86_64"* ]]; then
-        echo -e "\033[33mArchitecture $arch may be unsupported, will try to install the amd64 one anyway."
-    fi
-    arch=""
-fi
+determine_os_and_arch
 
 mkdir -p $BASE_DIR 
 
@@ -45,83 +53,46 @@ DOWNLOAD_URL="https://github.com/kubescape/kubescape/releases/${RELEASE}/kubesca
 
 curl --progress-bar -L $DOWNLOAD_URL -o $OUTPUT
 
-# Find install dir
-install_dir=/usr/local/bin # default if running as root
-if [ "$(id -u)" -ne 0 ]; then
-  install_dir=$BASE_DIR/bin # if not running as root, install to user dir
-  export PATH=$PATH:$BASE_DIR/bin
-fi
+# Determine install directory
+install_dir=/usr/local/bin
+[ "$(id -u)" -ne 0 ] && install_dir=$BASE_DIR/bin && export PATH=$PATH:$BASE_DIR/bin
 
 # Create install dir if it does not exist
-if [ ! -d "$install_dir" ]; then
-  mkdir -p $install_dir
-fi
+mkdir -p $install_dir
 
-chmod +x $OUTPUT 2>/dev/null
+chmod +x $OUTPUT
 
-# cleaning up old install
-SUDO=
-if [ "$(id -u)" -ne 0 ] && [ -n "$(which sudo)" ] && [ "$KUBESCAPE_EXEC" != "" ] && [ -f /usr/local/bin/$KUBESCAPE_EXEC ]; then
-    SUDO=sudo
-    echo -e "\n\033[33mOld installation as root found, do you want to remove it? [\033[0my\033[33m/n]:"
-    read -n 1 -r
-    if [[ ! $REPLY =~ ^[Yy]$ ]] && [[ "$REPLY" != "" ]]; then
-        echo -e "\n\033[0mSkipping old installation as root removal."
-    else
-        echo -e "\n\033[0mWe will need the root access to uninstall the old kubescape CLI."
-        if $SUDO rm -f /usr/local/bin/$KUBESCAPE_EXEC 2>/dev/null; then
-            echo -e "\033[32mRemoved old installation as root at /usr/local/bin/$KUBESCAPE_EXEC"
-        else
-            echo -e "\033[31mFailed to remove old installation as root at /usr/local/bin/$KUBESCAPE_EXEC, please remove it manually."
-        fi
-    fi
-fi
+# Remove old installations
+SUDO=""
+[ "$(id -u)" -ne 0 ] && [ -n "$(which sudo)" ] && [ -f /usr/local/bin/$KUBESCAPE_EXEC ] && SUDO=sudo
 
-if [ "$KUBESCAPE_EXEC" != "" ]; then
-    if [ "${SUDO_USER:-$USER}" != "" ]; then
-        rm -f /home/"${SUDO_USER:-$USER}"/.kubescape/bin/$KUBESCAPE_EXEC 2>/dev/null || true
-    fi
-    if [ "$BASE_DIR" != "" ]; then
-        rm -f $BASE_DIR/bin/$KUBESCAPE_EXEC 2>/dev/null || true
-    fi
-fi
+$SUDO remove_old_install "/usr/local/bin/$KUBESCAPE_EXEC"
+remove_old_install "$BASE_DIR/bin/$KUBESCAPE_EXEC"
 
-# Old install location, clean all those things up
-for pdir in ${PATH//:/ }; do
-    edir="${pdir/#\~/$HOME}"
-    if [[ $edir == $HOME/* ]] && [[ -f $edir/$KUBESCAPE_EXEC ]]; then
-        echo -e "\n\033[33mOld installation found at $edir/$KUBESCAPE_EXEC, do you want to remove it? [\033[0my\033[33m/n]:"
-        read -n 1 -r
-        if [[ ! $REPLY =~ ^[Yy]$ ]] && [[ "$REPLY" != "" ]]; then
-            continue
-        fi
-        if rm -f "$edir"/$KUBESCAPE_EXEC 2>/dev/null; then
-            echo -e "\n\033[32mRemoved old installation at $edir/$KUBESCAPE_EXEC"
-        else
-            echo -e "\n\033[31mFailed to remove old installation as root at $edir/$KUBESCAPE_EXEC, please remove it manually."
-        fi
-    fi
+# Remove any old installations in user's PATH
+IFS=':' read -ra ADDR <<< "$PATH"
+for pdir in "${ADDR[@]}"; do
+    remove_old_install "$pdir/$KUBESCAPE_EXEC"
 done
 
-cp $OUTPUT $install_dir/$KUBESCAPE_EXEC
-rm -f $OUTPUT
+# Move the new executable to the install directory
+mv $OUTPUT $install_dir/$KUBESCAPE_EXEC
 
-echo
 echo -e "\033[32mFinished Installation."
 
 if [ "$(id -u)" -ne 0 ]; then
-  echo -e "\nRemember to add the Kubescape CLI to your path with:"
-  echo -e "  export PATH=\$PATH:$BASE_DIR/bin"
-  export PATH=\$PATH:$BASE_DIR/bin
+  echo -e "\033[1;35;32m\nRemember to add the Kubescape CLI to your path with:"
+  echo -e "\033[1;35;40m$ export PATH=\$PATH:$BASE_DIR/bin"
 fi
 
 # Check cluster access by getting nodes
 if ! kubectl get nodes &> /dev/null; then
-    echo -e "  $KUBESCAPE_EXEC scan --create-account"
+    echo -e "\033[32m\nRun:"
+    echo -e "\033[1;35;40m$ $KUBESCAPE_EXEC scan --create-account"
+    echo
     exit 0
 fi
 
-echo -e "\033[0m"
+echo -e "\033[0;37;40m"
 echo -e "\033[32mExecuting Kubescape."
-echo
 $KUBESCAPE_EXEC scan --create-account
