1844aaf361f093261d9414a955598895d292ef8b
Amir Malka <amirm@armosec.io>
2023-08-29 18:27:46 +0300

    fix
    Signed-off-by: Amir Malka <amirm@armosec.io>

--- core/cautils/scaninfo_test.go
+++ core/cautils/scaninfo_test.go
@@ -1,12 +1,40 @@
 package cautils
 
 import (
+	"context"
 	"os"
 	"path/filepath"
 	"testing"
 
+	reporthandlingv2 "github.com/kubescape/opa-utils/reporthandling/v2"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestSetContextMetadata(t *testing.T) {
+	{
+		ctx := reporthandlingv2.ContextMetadata{}
+		setContextMetadata(context.TODO(), &ctx, "")
+
+		assert.NotNil(t, ctx.ClusterContextMetadata)
+		assert.Nil(t, ctx.DirectoryContextMetadata)
+		assert.Nil(t, ctx.FileContextMetadata)
+		assert.Nil(t, ctx.HelmContextMetadata)
+		assert.Nil(t, ctx.RepoContextMetadata)
+	}
+	// TODO: tests were commented out due to actual http calls ; http calls should be mocked.
+	/*{
+		ctx := reporthandlingv2.ContextMetadata{}
+		setContextMetadata(&ctx, "https://github.com/kubescape/kubescape")
+		assert.Nil(t, ctx.ClusterContextMetadata)
+		assert.Nil(t, ctx.DirectoryContextMetadata)
+		assert.Nil(t, ctx.FileContextMetadata)
+		assert.Nil(t, ctx.HelmContextMetadata)
+		assert.NotNil(t, ctx.RepoContextMetadata)
+		assert.Equal(t, "kubescape", ctx.RepoContextMetadata.Repo)
+		assert.Equal(t, "kubescape", ctx.RepoContextMetadata.Owner)
+		assert.Equal(t, "master", ctx.RepoContextMetadata.Branch)
+	}*/
+}
 
 func TestGetHostname(t *testing.T) {
 	// Test that the hostname is not empty
