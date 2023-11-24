f86fa993169c438d35075cb1e4a5bb4ebf050674
cbrom <kb.cbrom@gmail.com>
2023-11-25 00:59:45 +0300

    reverted core/cautils/getter/json.go file, as the json and init of json is necessary
    Signed-off-by: cbrom <kb.cbrom@gmail.com>

---
+++ core/cautils/getter/json.go
@@ -0,0 +1,13 @@
+package getter
+
+import (
+	jsoniter "github.com/json-iterator/go"
+)
+
+var json jsoniter.API
+
+func init() {
+	// NOTE(fredbi): attention, this configuration rounds floats down to 6 digits
+	// For finer-grained config, see: https://pkg.go.dev/github.com/json-iterator/go#section-readme
+	json = jsoniter.ConfigFastest
+}
