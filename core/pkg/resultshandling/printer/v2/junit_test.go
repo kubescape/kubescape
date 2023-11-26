e4477eaac425cfb7daf1df673531140a06a5c774
VaibhavMalik4187 <vaibhavmalik2018@gmail.com>
2023-11-26 17:00:58 +0530

    Fixed invalid input bug, added tests for printers
    Added tests in the jsonprinter.go, junit.go pdf.go prometheus.go files
    and updated the `Score` functions to handle invalid scores.
    
    Signed-off-by: VaibhavMalik4187 <vaibhavmalik2018@gmail.com>

---
+++ core/pkg/resultshandling/printer/v2/junit_test.go
@@ -0,0 +1,91 @@
+package printer
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJunitPdfPrinter(t *testing.T) {
+	// Verbose mode off
+	jp := NewJunitPrinter(false)
+	assert.NotNil(t, jp)
+	assert.Equal(t, false, jp.verbose)
+
+	// Verbose mode on
+	jp = NewJunitPrinter(true)
+	assert.NotNil(t, jp)
+	assert.Equal(t, true, jp.verbose)
+}
+
+func TestScore_Junit(t *testing.T) {
+	tests := []struct {
+		name  string
+		score float32
+		want  string
+	}{
+		{
+			name:  "Score not an integer",
+			score: 20.7,
+			want:  "\nOverall compliance-score (100- Excellent, 0- All failed): 21\n",
+		},
+		{
+			name:  "Score less than 0",
+			score: -20.0,
+			want:  "\nOverall compliance-score (100- Excellent, 0- All failed): 0\n",
+		},
+		{
+			name:  "Score greater than 100",
+			score: 120.0,
+			want:  "\nOverall compliance-score (100- Excellent, 0- All failed): 100\n",
+		},
+		{
+			name:  "Score 50",
+			score: 50.0,
+			want:  "\nOverall compliance-score (100- Excellent, 0- All failed): 50\n",
+		},
+		{
+			name:  "Zero Score",
+			score: 0.0,
+			want:  "\nOverall compliance-score (100- Excellent, 0- All failed): 0\n",
+		},
+		{
+			name:  "Perfect Score",
+			score: 100,
+			want:  "\nOverall compliance-score (100- Excellent, 0- All failed): 100\n",
+		},
+	}
+
+	jp := NewJunitPrinter(false)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Create a temporary file to capture output
+			f, err := os.CreateTemp("", "score-output")
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+
+			// Redirect stderr to the temporary file
+			oldStderr := os.Stderr
+			defer func() {
+				os.Stderr = oldStderr
+			}()
+			os.Stderr = f
+
+			// Print the score using the `Score` function
+			jp.Score(tt.score)
+
+			// Read the contents of the temporary file
+			f.Seek(0, 0)
+			got, err := ioutil.ReadAll(f)
+			if err != nil {
+				panic(err)
+			}
+			assert.Equal(t, tt.want, string(got))
+		})
+	}
+}
