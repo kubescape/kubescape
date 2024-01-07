0496291cae2db945d5f35a9f82776f94293fbfd9
David Wertenteil <dwertent@armosec.io>
2024-01-07 22:58:03 +0200

    remove unstable tests
    Signed-off-by: David Wertenteil <dwertent@armosec.io>

--- core/pkg/resultshandling/printer/printresults_test.go
+++ core/pkg/resultshandling/printer/printresults_test.go
@@ -14,12 +14,3 @@ func TestGetWriter_EmptyFileName(t *testing.T) {
 	file := GetWriter(ctx, outputFile)
 	assert.Equal(t, os.Stdout, file)
 }
-
-func TestGetWriter_NonEmptyFileName(t *testing.T) {
-	ctx := context.Background()
-	outputFile := "temp.txt"
-	file := GetWriter(ctx, outputFile)
-	assert.NotEqual(t, os.Stdout, file)
-	err := os.Remove(outputFile)
-	assert.Nil(t, err)
-}
