d742b9ff9d1d1edac5fa2b28ae822cf632bae10d
Amit Schendel <amitschendel@gmail.com>
2024-01-31 10:47:49 +0200

    Adding timeout for service discovery
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>

--- core/pkg/opaprocessor/networkscanner.go
+++ core/pkg/opaprocessor/networkscanner.go
@@ -2,16 +2,19 @@ package opaprocessor
 
 import (
 	"fmt"
+	"time"
 
 	servicediscovery "github.com/kubescape/kubescape-network-scanner/cmd"
 )
 
 const (
 	ServiceSuffix = ".svc.cluster.local"
+	Timeout       = time.Second * 3
 )
 
 // Check if the service is unauthenticated using kubescape-network-scanner.
 func isUnauthenticatedService(host string, port int, namespace string) bool {
+	// Skip kube-system namespace.
 	if namespace == "kube-system" {
 		return false
 	}
@@ -22,14 +25,23 @@ func isUnauthenticatedService(host string, port int, namespace string) bool {
 
 	k8sService := fmt.Sprintf("%s.%s%s", host, namespace, ServiceSuffix)
 
-	discoveryResults, err := servicediscovery.ScanTargets(k8sService, port)
-	if err != nil {
+	// Run the network scanner in a goroutine and wait for the result.
+	results := make(chan bool, 1)
+	go func() {
+		discoveryResults, err := servicediscovery.ScanTargets(k8sService, port)
+		if err != nil {
+			results <- false
+		} else if !discoveryResults.IsAuthenticated && discoveryResults.ApplicationLayer != "" {
+			results <- true
+		}
+
+		results <- false
+	}()
+
+	select {
+	case result := <-results:
+		return result
+	case <-time.After(Timeout):
 		return false
 	}
-
-	if !discoveryResults.IsAuthenticated && discoveryResults.ApplicationLayer != "" {
-		return true
-	}
-
-	return false
 }
