fa58dd167cab173d2b5c95c7dfde24b6bba03630
Anvay <anvaykharb007@gmail.com>
2026-05-15 21:32:15 +0530

    test(imagescan): cover DB load validation
    Signed-off-by: Anvay <anvaykharb007@gmail.com>

--- pkg/imagescan/imagescan_test.go
+++ pkg/imagescan/imagescan_test.go
@@ -1,6 +1,7 @@
 package imagescan
 
 import (
+	"errors"
 	"strings"
 	"testing"
 
@@ -247,6 +248,49 @@ func TestNewDefaultDBConfig(t *testing.T) {
 				assert.NotEmpty(t, installCfg.DBRootDir)
 				assert.True(t, strings.HasSuffix(installCfg.DBRootDir, defaultDBDirName))
 			}
+		})
+	}
+}
+
+func TestValidateDBLoad(t *testing.T) {
+	tests := []struct {
+		name    string
+		loadErr error
+		status  *vulnerability.ProviderStatus
+		wantErr string
+	}{
+		{
+			name:    "load error is wrapped",
+			loadErr: errors.New("boom"),
+			wantErr: "failed to load vulnerability db: boom",
+		},
+		{
+			name:    "nil status is rejected",
+			wantErr: "unable to determine the status of the vulnerability db",
+		},
+		{
+			name: "status error is wrapped",
+			status: &vulnerability.ProviderStatus{
+				Error: errors.New("status failure"),
+			},
+			wantErr: "db could not be loaded: status failure",
+		},
+		{
+			name:   "valid status passes",
+			status: &vulnerability.ProviderStatus{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := validateDBLoad(tt.loadErr, tt.status)
+			if tt.wantErr == "" {
+				require.NoError(t, err)
+				return
+			}
+
+			require.Error(t, err)
+			assert.EqualError(t, err, tt.wantErr)
 		})
 	}
 }
