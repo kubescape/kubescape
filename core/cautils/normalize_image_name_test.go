9e891477332850565e88790a5deaa36f05d8efc2
Anvay <anvaykharb007@gmail.com>
2026-05-16 22:41:05 +0530

    test(cautils): expand normalize image name tests
    Signed-off-by: Anvay <anvaykharb007@gmail.com>

--- core/cautils/normalize_image_name_test.go
+++ core/cautils/normalize_image_name_test.go
@@ -9,20 +9,45 @@ import (
 func Test_normalize_name(t *testing.T) {
 
 	tests := []struct {
-		name string
-		img  string
-		want string
+		name    string
+		img     string
+		want    string
+		wantErr bool
 	}{
 		{
-			name: "Normalize image name",
-			img:  "nginx",
-			want: "docker.io/library/nginx",
+			name:    "Normalize simple image name",
+			img:     "nginx",
+			want:    "docker.io/library/nginx",
+			wantErr: false,
+		},
+		{
+			name:    "Normalize image with tag",
+			img:     "nginx:latest",
+			want:    "docker.io/library/nginx:latest",
+			wantErr: false,
+		},
+		{
+			name:    "Normalize image with custom registry",
+			img:     "quay.io/coreos/etcd:v3.5",
+			want:    "quay.io/coreos/etcd:v3.5",
+			wantErr: false,
+		},
+		{
+			name:    "Invalid image name",
+			img:     "https://docker.io/library/nginx",
+			want:    "",
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			name, _ := NormalizeImageName(tt.img)
-			assert.Equal(t, tt.want, name, tt.name)
+			name, err := NormalizeImageName(tt.img)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want, name, tt.name)
+			}
 		})
 	}
 }
