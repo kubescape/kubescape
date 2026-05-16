2c5dc9cf15137019af07a41013d24d8021d2d85a
Anvay <anvaykharb007@gmail.com>
2026-05-16 22:42:42 +0530

    test(cautils): expand floatutils tests
    Signed-off-by: Anvay <anvaykharb007@gmail.com>

--- core/cautils/floatutils_test.go
+++ core/cautils/floatutils_test.go
@@ -10,15 +10,28 @@ func TestFloat64ToInt(t *testing.T) {
 	assert.Equal(t, 3, Float64ToInt(3.49))
 	assert.Equal(t, 4, Float64ToInt(3.5))
 	assert.Equal(t, 4, Float64ToInt(3.51))
+	assert.Equal(t, 0, Float64ToInt(0.0))
+	assert.Equal(t, -3, Float64ToInt(-3.49))
+	assert.Equal(t, -4, Float64ToInt(-3.5))
+	assert.Equal(t, -4, Float64ToInt(-3.51))
 }
 
 func TestFloat32ToInt(t *testing.T) {
 	assert.Equal(t, 3, Float32ToInt(3.49))
 	assert.Equal(t, 4, Float32ToInt(3.5))
 	assert.Equal(t, 4, Float32ToInt(3.51))
+	assert.Equal(t, 0, Float32ToInt(0.0))
+	assert.Equal(t, -3, Float32ToInt(-3.49))
+	assert.Equal(t, -4, Float32ToInt(-3.5))
+	assert.Equal(t, -4, Float32ToInt(-3.51))
 }
+
 func TestFloat16ToInt(t *testing.T) {
 	assert.Equal(t, 3, Float16ToInt(3.49))
 	assert.Equal(t, 4, Float16ToInt(3.5))
 	assert.Equal(t, 4, Float16ToInt(3.51))
+	assert.Equal(t, 0, Float16ToInt(0.0))
+	assert.Equal(t, -3, Float16ToInt(-3.49))
+	assert.Equal(t, -4, Float16ToInt(-3.5))
+	assert.Equal(t, -4, Float16ToInt(-3.51))
 }
