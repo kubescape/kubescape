8fe067d62f1258b569bfb8576f70c8840cd5f196
Daksh Pathak <daksh.pathak.ug24@nsut.ac.in>
2026-05-16 18:18:25 +0530

    test(metrics): assert Init registers counters
    Signed-off-by: Daksh Pathak <daksh.pathak.ug24@nsut.ac.in>

--- core/metrics/metrics_test.go
+++ core/metrics/metrics_test.go
@@ -2,22 +2,46 @@ package metrics
 
 import (
 	"context"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
+func resetMetricsForTest(t *testing.T) {
+	t.Helper()
+
+	savedInitOnce := initOnce
+	savedKubernetesResourcesCount := kubernetesResourcesCount
+	savedWorkerNodesCount := workerNodesCount
+
+	initOnce = sync.Once{}
+	kubernetesResourcesCount = nil
+	workerNodesCount = nil
+
+	t.Cleanup(func() {
+		initOnce = savedInitOnce
+		kubernetesResourcesCount = savedKubernetesResourcesCount
+		workerNodesCount = savedWorkerNodesCount
+	})
+}
+
 func TestInit(t *testing.T) {
+	resetMetricsForTest(t)
+
 	// Init should be safe to call multiple times thanks to sync.Once.
 	assert.NotPanics(t, func() { Init() })
+	require.NotNil(t, kubernetesResourcesCount)
+	require.NotNil(t, workerNodesCount)
+
 	assert.NotPanics(t, func() { Init() })
+	require.NotNil(t, kubernetesResourcesCount)
+	require.NotNil(t, workerNodesCount)
 }
 
 func TestUpdateKubernetesResourcesCount_NilCounter(t *testing.T) {
-	// Before Init(), the counter is nil. Update should not panic.
-	saved := kubernetesResourcesCount
-	kubernetesResourcesCount = nil
-	defer func() { kubernetesResourcesCount = saved }()
+	resetMetricsForTest(t)
 
 	assert.NotPanics(t, func() {
 		UpdateKubernetesResourcesCount(context.Background(), 5)
@@ -25,10 +49,7 @@ func TestUpdateKubernetesResourcesCount_NilCounter(t *testing.T) {
 }
 
 func TestUpdateWorkerNodesCount_NilCounter(t *testing.T) {
-	// Before Init(), the counter is nil. Update should not panic.
-	saved := workerNodesCount
-	workerNodesCount = nil
-	defer func() { workerNodesCount = saved }()
+	resetMetricsForTest(t)
 
 	assert.NotPanics(t, func() {
 		UpdateWorkerNodesCount(context.Background(), 3)
@@ -36,16 +57,22 @@ func TestUpdateWorkerNodesCount_NilCounter(t *testing.T) {
 }
 
 func TestUpdateKubernetesResourcesCount_AfterInit(t *testing.T) {
+	resetMetricsForTest(t)
+
 	Init()
-	// After Init(), the counter is set. Update should not panic.
+	require.NotNil(t, kubernetesResourcesCount)
+
 	assert.NotPanics(t, func() {
 		UpdateKubernetesResourcesCount(context.Background(), 10)
 	})
 }
 
 func TestUpdateWorkerNodesCount_AfterInit(t *testing.T) {
+	resetMetricsForTest(t)
+
 	Init()
-	// After Init(), the counter is set. Update should not panic.
+	require.NotNil(t, workerNodesCount)
+
 	assert.NotPanics(t, func() {
 		UpdateWorkerNodesCount(context.Background(), 4)
 	})
