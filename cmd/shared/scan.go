ec0e07539c7e52f56aee1f23a2807ee4af27c81e
Shreya2005-2005 <bhakatmistu@email.com>
2026-05-18 00:18:48 +0000

    fix: validate threshold ranges in scan image and patch commands Fixes #2271
    Signed-off-by: Shreya2005-2005 <bhakatmistu@email.com>

--- cmd/shared/scan.go
+++ cmd/shared/scan.go
@@ -2,6 +2,7 @@ package shared
 
 import (
 	"fmt"
+	"math"
 	"strings"
 
 	"github.com/kubescape/go-logger/helpers"
@@ -10,6 +11,25 @@ import (
 )
 
 var ErrUnknownSeverity = fmt.Errorf("unknown severity. Supported severities are: %s", strings.Join(reporthandlingapis.GetSupportedSeverities(), ", "))
+
+// ErrBadThreshold is returned when a numeric threshold is outside the valid range [0, 100].
+var ErrBadThreshold = fmt.Errorf("bad argument: out of range threshold")
+
+// ValidateThresholds validates that FailThreshold, ComplianceThreshold and
+// FailCoverageThreshold are all within [0, 100]. This mirrors the check in
+// validateFrameworkScanInfo and validateControlScanInfo.
+func ValidateThresholds(scanInfo *cautils.ScanInfo) error {
+	if math.IsNaN(float64(scanInfo.FailThreshold)) || 100 < scanInfo.FailThreshold || 0 > scanInfo.FailThreshold {
+		return ErrBadThreshold
+	}
+	if math.IsNaN(float64(scanInfo.ComplianceThreshold)) || 100 < scanInfo.ComplianceThreshold || 0 > scanInfo.ComplianceThreshold {
+		return ErrBadThreshold
+	}
+	if math.IsNaN(float64(scanInfo.FailCoverageThreshold)) || 100 < scanInfo.FailCoverageThreshold || 0 > scanInfo.FailCoverageThreshold {
+		return ErrBadThreshold
+	}
+	return nil
+}
 
 // ValidateSeverity returns an error if a given severity is not known, nil otherwise
 func ValidateSeverity(severity string) error {
