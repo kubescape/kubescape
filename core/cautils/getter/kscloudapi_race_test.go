ce7341e8f21d71748a002e106c9961b236697d1f
Lalit Kishore <lr_be24@thapar.edu>
2026-08-06 10:17:50 +0530

    test: make race test deterministic and restore state
    Signed-off-by: Lalit Kishore <lr_be24@thapar.edu>

--- core/cautils/getter/kscloudapi_race_test.go
+++ core/cautils/getter/kscloudapi_race_test.go
@@ -8,12 +8,32 @@ import (
 )
 
 func TestKSCloudAPIConnector_Race(t *testing.T) {
+	// Save and restore global state
+	globalKSCloudAPIConnectorMutex.RLock()
+	original := globalKSCloudAPIConnector
+	globalKSCloudAPIConnectorMutex.RUnlock()
+
+	t.Cleanup(func() {
+		globalKSCloudAPIConnectorMutex.Lock()
+		globalKSCloudAPIConnector = original
+		globalKSCloudAPIConnectorMutex.Unlock()
+	})
+
+	// Reset to nil to test lazy initialization path
+	globalKSCloudAPIConnectorMutex.Lock()
+	globalKSCloudAPIConnector = nil
+	globalKSCloudAPIConnectorMutex.Unlock()
+
 	var wg sync.WaitGroup
+	var startBarrier sync.WaitGroup
+
 	wg.Add(2)
+	startBarrier.Add(1)
 
 	// Concurrent writer
 	go func() {
 		defer wg.Done()
+		startBarrier.Wait()
 		for i := 0; i < 100000; i++ {
 			SetKSCloudAPIConnector(v1.NewEmptyKSCloudAPI())
 		}
@@ -22,10 +42,14 @@ func TestKSCloudAPIConnector_Race(t *testing.T) {
 	// Concurrent reader
 	go func() {
 		defer wg.Done()
+		startBarrier.Wait()
 		for i := 0; i < 100000; i++ {
 			_ = GetKSCloudAPIConnector()
 		}
 	}()
+
+	// Release the barrier to start both goroutines simultaneously
+	startBarrier.Done()
 
 	wg.Wait()
 }
