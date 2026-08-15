23d823d4a0b31dd6d3391712534aa5c87f2b66b0
Rucha0901 <imt_2025071@iiitm.ac.in>
2026-08-15 21:04:41 +0530

    test: fix missing import for hostsensorutils
    Signed-off-by: Rucha0901 <imt_2025071@iiitm.ac.in>

--- core/pkg/resourcehandler/k8sresources_pull_test.go
+++ core/pkg/resourcehandler/k8sresources_pull_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/kubescape/k8s-interface/k8sinterface"
 	"github.com/kubescape/kubescape/v3/core/cautils"
+	"github.com/kubescape/kubescape/v3/core/pkg/hostsensorutils"
 	"github.com/kubescape/opa-utils/objectsenvelopes/hostsensor"
 	"github.com/kubescape/opa-utils/reporthandling"
 	"github.com/kubescape/opa-utils/reporthandling/apis"
