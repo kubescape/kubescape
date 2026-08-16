dbf0eb7fe6b75d5c14bd51137063c6e8583b73da
Karthik Rajan <karthikrajanmr@gmail.com>
2026-08-16 10:18:42 +0530

    fix(core): use case-insensitive RCE keyword check in IsRCE
    Signed-off-by: Karthik Rajan <karthikrajanmr@gmail.com>

--- core/pkg/containerscan/datastructuresmethods.go
+++ core/pkg/containerscan/datastructuresmethods.go
@@ -39,7 +39,7 @@ func (scanresult *ScanResultReport) Validate() bool {
 func (v *Vulnerability) IsRCE() bool {
 	desc := strings.ToLower(v.Description)
 
-	isRCE := strings.Contains(v.Description, "RCE")
+	isRCE := strings.Contains(desc, "rce")
 
 	return isRCE || strings.Contains(desc, "remote code execution") || strings.Contains(desc, "remote command execution") || strings.Contains(desc, "arbitrary code") || strings.Contains(desc, "code execution") || strings.Contains(desc, "code injection") || strings.Contains(desc, "command injection") || strings.Contains(desc, "inject arbitrary commands")
 }
