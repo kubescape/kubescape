9dc3aef1cecc2886c4c8c07a0e00dd38eacf2974
Naitik Rishu <naitik.2111.nr@gmail.com>
2026-08-20 22:08:32 +0530

    fix(printer): support multiple image SBOM generation in CycloneDX and SPDX printers (#3399)
    Signed-off-by: Naitik Rishu <naitikrishu@Naitiks-MacBook-Air.local>

--- core/pkg/resultshandling/printer/v2/cyclonedxprinter.go
+++ core/pkg/resultshandling/printer/v2/cyclonedxprinter.go
@@ -2,6 +2,7 @@ package printer
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -57,10 +58,6 @@ func (cp *CycloneDXPrinter) ActionPrint(ctx context.Context, opaSessionObj *caut
 		logger.L().Ctx(ctx).Error("cyclonedx-json output is only supported for image scans")
 		return fmt.Errorf("cyclonedx-json output is only supported for image scans")
 	}
-	if imageScanData[0].SBOM == nil {
-		logger.L().Ctx(ctx).Error("no SBOM data available for cyclonedx-json output")
-		return fmt.Errorf("no SBOM data available for cyclonedx-json output")
-	}
 
 	encoder, err := cyclonedxjson.NewFormatEncoderWithConfig(cyclonedxjson.DefaultEncoderConfig())
 	if err != nil {
@@ -68,13 +65,39 @@ func (cp *CycloneDXPrinter) ActionPrint(ctx context.Context, opaSessionObj *caut
 		return err
 	}
 
-	data, err := format.Encode(*imageScanData[0].SBOM, encoder)
-	if err != nil {
-		logger.L().Ctx(ctx).Error("failed to encode SBOM as cyclonedx-json", helpers.Error(err))
-		return err
+	var encodedDocs []json.RawMessage
+	for _, scan := range imageScanData {
+		if scan.SBOM == nil {
+			logger.L().Ctx(ctx).Warning("skipping image with no SBOM data available for cyclonedx-json output")
+			continue
+		}
+
+		data, err := format.Encode(*scan.SBOM, encoder)
+		if err != nil {
+			logger.L().Ctx(ctx).Error("failed to encode SBOM as cyclonedx-json", helpers.Error(err))
+			return err
+		}
+
+		encodedDocs = append(encodedDocs, json.RawMessage(data))
 	}
 
-	if _, err := cp.writer.Write(data); err != nil {
+	if len(encodedDocs) == 0 {
+		logger.L().Ctx(ctx).Error("no SBOM data available for cyclonedx-json output")
+		return fmt.Errorf("no SBOM data available for cyclonedx-json output")
+	}
+
+	var outputBytes []byte
+	if len(encodedDocs) == 1 {
+		outputBytes = encodedDocs[0]
+	} else {
+		outputBytes, err = json.MarshalIndent(encodedDocs, "", "  ")
+		if err != nil {
+			logger.L().Ctx(ctx).Error("failed to marshal multi-image cyclonedx-json output", helpers.Error(err))
+			return err
+		}
+	}
+
+	if _, err := cp.writer.Write(outputBytes); err != nil {
 		logger.L().Ctx(ctx).Error("failed to write cyclonedx-json output", helpers.Error(err))
 		return err
 	}
