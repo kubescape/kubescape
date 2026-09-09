04486052e526b4cf947b941f4c668773d44fa942
Komal <komal@example.com>
2026-09-09 10:39:01 +0530

    Refactor: Move getReadableID to k8sinterface
    Signed-off-by: Komal <komal@example.com>

--- core/pkg/resourcehandler/filesloaderutils.go
+++ core/pkg/resourcehandler/filesloaderutils.go
@@ -165,26 +165,12 @@ func findScanObjectResource(mappedResources map[string][]workloadinterface.IMeta
 	}
 
 	if len(wls) == 0 {
-		return nil, fmt.Errorf("k8s resource '%s' not found", getReadableID(resource))
+		return nil, fmt.Errorf("k8s resource '%s' not found", k8sinterface.GetReadableID(resource))
 	} else if len(wls) > 1 {
-		return nil, fmt.Errorf("more than one k8s resource found for '%s'", getReadableID(resource))
+		return nil, fmt.Errorf("more than one k8s resource found for '%s'", k8sinterface.GetReadableID(resource))
 	}
 
 	return wls[0], nil
 }
 
-// TODO: move this to k8s-interface
-func getReadableID(obj *objectsenvelopes.ScanObject) string {
-	var ID string
-	if obj.GetApiVersion() != "" {
-		ID += fmt.Sprintf("%s/", k8sinterface.JoinGroupVersion(k8sinterface.SplitApiVersion(obj.GetApiVersion())))
-	}
 
-	if obj.GetNamespace() != "" {
-		ID += fmt.Sprintf("%s/", obj.GetNamespace())
-	}
-
-	ID += fmt.Sprintf("%s/%s", obj.GetKind(), obj.GetName())
-
-	return ID
-}
