1f3ba841291f397f99c6c0c10e453a5b686e0df8
Gaurav Chaudhary <chaudharygaurav2004@gmail.com>
2026-09-11 23:00:24 +0530

    test(mcpserver): assert rejected argument details
    Signed-off-by: Gaurav Chaudhary <chaudharygaurav2004@gmail.com>

--- cmd/mcpserver/mcpserver_unit_test.go
+++ cmd/mcpserver/mcpserver_unit_test.go
@@ -317,6 +317,7 @@ func TestGetConfigurationDrift_WhitespaceArgumentsRejected(t *testing.T) {
 			var toolErr ToolError
 			require.NoError(t, json.Unmarshal([]byte(toolResultText(t, result)), &toolErr))
 			assert.Equal(t, ErrCodeInvalidArgument, toolErr.Code)
+			assert.Equal(t, arg, toolErr.Details["argument"])
 		})
 	}
 }
